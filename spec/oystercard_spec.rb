0f4d4ebcd44d7e70f82b1e6c67ac892a95ad2f12
Student <student@Makerss-MacBook-Pro.local>
2019-08-27 15:18:01 +0100

    Added code and tests for new top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,5 +4,9 @@ describe Oystercard do
   it 'starts with a zero balance' do
     expect(subject.balance).to eq(0)
   end
-
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+  it 'updates balance with top up amount' do
+    subject.top_up(5)
+    expect(subject.balance).to eq(5)
+  end
 end
